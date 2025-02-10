4499b20fc2de09102e104bf2b3d62d370ba7847c
MahmoudHanyFathlla <mahmoudhanyfathalla@gmail.com>
2025-02-10 17:38:42 +0200

    add data

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+# Read the README file for long_description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dooGL",  # The name of your package
-    version="0.2.1",  # Initial version
+    version="0.2.2",  # Initial version
     packages=find_packages(),  # Automatically discover all modules
     python_requires='>=3.6',  # Minimum Python version requirement
     include_package_data=True,  # Automatically include files specified in package_data
@@ -16,4 +20,6 @@ setup(
         'Pillow',          # Python Imaging Library (PIL fork)
         # Add any other dependencies your project requires here
     ],
+     long_description=long_description,  # Use the content of README.md
+     long_description_content_type="text/markdown",  # Specify that README is in Markdown format
 )
